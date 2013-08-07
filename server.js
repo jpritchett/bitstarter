abc58e758801d3c7bebb37bee2ec0c2d6313a8f0
mightymoonlord <mightymoonlord@gmail.com>
2013-08-07 18:30:51 +0100

    Change port of server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var static = require('node-static'),
   http = require('http'),
   util = require('util');
 var webroot =__dirname,
-  port = 81;
+  port = 80;
 
 console.log(webroot);
 var file = new(static.Server)(webroot, {
